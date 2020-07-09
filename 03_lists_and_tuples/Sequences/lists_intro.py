bf294475a1c48a4c3c631bd57f1c2744beaf2358
Louie Fitzpatrick <fitz4492@gmail.com>
2020-07-08 23:00:39 -0500

    started new dir for lists and tuples

---
+++ 03_lists_and_tuples/Sequences/lists_intro.py
@@ -0,0 +1,13 @@
+computer_parts = ["computer",
+                  "monitor",
+                  "keyboard",
+                  "mouse"
+                  ]
+
+for part in computer_parts:
+    print(part)
+
+print()
+print("The computer part at index 1 is the " + computer_parts[1])
+print("The computer part at index -1 is the " + computer_parts[-1])
+print("The computer part at index 0:3 is the ", computer_parts[0:3])
