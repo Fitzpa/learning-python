ed4a91e664731f1aae56899d0ac5d4a668c3b9ca
Louie Fitzpatrick <fitz4492@gmail.com>
2020-07-14 19:14:55 -0500

    sorting lists

--- 03_lists_and_tuples/number_lists.py
+++ 03_lists_and_tuples/number_lists.py
@@ -14,3 +14,13 @@ print(len(odd))
 
 print()
 print("Mississippi".count("i"))
+
+even.extend(odd)
+print('even ', even)
+print()
+even.sort()
+print('even sorted ascending', even)
+even.sort(reverse=True)
+print('even sorted descending', even)
+# Lists are mutable
+# The sort() method doesn't create a copy of the list. It just rearranges the order of the list in place.
